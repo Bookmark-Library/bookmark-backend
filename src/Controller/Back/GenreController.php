6528de4da23655df5de904452b877cec14075d36
Laurent-Finana <laurentfinanago@gmail.com>
2023-04-14 09:34:53 +0200

    refacto back folder Controller

--- src/Controller/Back/GenreController.php
+++ src/Controller/Back/GenreController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Back;
 
 use App\Entity\Genre;
 use App\Form\GenreType;
@@ -11,22 +11,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/genre")
+ * @Route("/back/genre")
  */
 class GenreController extends AbstractController
 {
     /**
-     * @Route("/", name="app_genre_index", methods={"GET"})
+     * @Route("/", name="app_back_genre_index", methods={"GET"})
      */
     public function index(GenreRepository $genreRepository): Response
     {
-        return $this->render('genre/index.html.twig', [
+        return $this->render('back/genre/index.html.twig', [
             'genres' => $genreRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="app_genre_new", methods={"GET", "POST"})
+     * @Route("/new", name="app_back_genre_new", methods={"GET", "POST"})
      */
     public function new(Request $request, GenreRepository $genreRepository): Response
     {
@@ -37,27 +37,27 @@ class GenreController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $genreRepository->add($genre, true);
 
-            return $this->redirectToRoute('app_genre_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_back_genre_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('genre/new.html.twig', [
+        return $this->renderForm('back/genre/new.html.twig', [
             'genre' => $genre,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_genre_show", methods={"GET"})
+     * @Route("/{id}", name="app_back_genre_show", methods={"GET"})
      */
     public function show(Genre $genre): Response
     {
-        return $this->render('genre/show.html.twig', [
+        return $this->render('back/genre/show.html.twig', [
             'genre' => $genre,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="app_genre_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="app_back_genre_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Genre $genre, GenreRepository $genreRepository): Response
     {
@@ -67,17 +67,17 @@ class GenreController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $genreRepository->add($genre, true);
 
-            return $this->redirectToRoute('app_genre_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_back_genre_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('genre/edit.html.twig', [
+        return $this->renderForm('back/genre/edit.html.twig', [
             'genre' => $genre,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_genre_delete", methods={"POST"})
+     * @Route("/{id}", name="app_back_genre_delete", methods={"POST"})
      */
     public function delete(Request $request, Genre $genre, GenreRepository $genreRepository): Response
     {
@@ -85,6 +85,6 @@ class GenreController extends AbstractController
             $genreRepository->remove($genre, true);
         }
 
-        return $this->redirectToRoute('app_genre_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_back_genre_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Back;
 
 use App\Entity\User;
 use App\Form\UserType;
@@ -11,22 +11,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/user")
+ * @Route("/back/user")
  */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/", name="app_user_index", methods={"GET"})
+     * @Route("/", name="app_back_user_index", methods={"GET"})
      */
     public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('back/user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="app_user_new", methods={"GET", "POST"})
+     * @Route("/new", name="app_back_user_new", methods={"GET", "POST"})
      */
     public function new(Request $request, UserRepository $userRepository): Response
     {
@@ -37,27 +37,27 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user, true);
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('user/new.html.twig', [
+        return $this->renderForm('back/user/new.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_user_show", methods={"GET"})
+     * @Route("/{id}", name="app_back_user_show", methods={"GET"})
      */
     public function show(User $user): Response
     {
-        return $this->render('user/show.html.twig', [
+        return $this->render('back/user/show.html.twig', [
             'user' => $user,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="app_user_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="app_back_user_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, User $user, UserRepository $userRepository): Response
     {
@@ -67,17 +67,17 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user, true);
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('user/edit.html.twig', [
+        return $this->renderForm('back/user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_user_delete", methods={"POST"})
+     * @Route("/{id}", name="app_back_user_delete", methods={"POST"})
      */
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
@@ -85,6 +85,6 @@ class UserController extends AbstractController
             $userRepository->remove($user, true);
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
