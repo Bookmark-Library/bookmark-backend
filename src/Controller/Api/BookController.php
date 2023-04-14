ea1f460f66df005dcbde9206e9ef61e9130c9916
Laurent-Finana <laurentfinanago@gmail.com>
2023-04-14 09:41:53 +0200

    Creation BookController and UserController

---
+++ src/Controller/Api/BookController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    /**
+     * Retourne les données en JSON
+     * 
+     * @Route("/api/books", name="app_api_get_books", methods={"GET"})
+     */
+    public function getBooks(BookRepository $bookRepository): Response
+    {
+        $books = $bookRepository->findAll();
+
+        // @see https://symfony.com/doc/5.4/controller.html#returning-json-response
+        return $this->json(
+            // données à convertir/serialiser
+            $books,
+            // status code
+            Response::HTTP_OK,
+            // header
+            [],
+            // options à transmettre au Serializer
+            ['groups' => 'get_books']
+        );
+    }
+}
