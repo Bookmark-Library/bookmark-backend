a1a5705921f99d1c09481bf1d06b197d36392567
Sandy-Bouzid <bouzid.sandy@gmail.com>
2023-04-17 13:58:02 +0200

    fixtures with truncate

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,33 +10,38 @@ use App\Entity\Library;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\DBAL\Connection;
 
 class AppFixtures extends Fixture
 {
+
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    private function truncate()
+    {
+        // On passe en mode SQL ! On cause avec MySQL
+        // Désactivation la vérification des contraintes FK
+        $this->connection->executeQuery('SET foreign_key_checks = 0');
+        // On tronque
+        $this->connection->executeQuery('TRUNCATE TABLE author');
+        $this->connection->executeQuery('TRUNCATE TABLE book');
+        $this->connection->executeQuery('TRUNCATE TABLE genre');
+        $this->connection->executeQuery('TRUNCATE TABLE library');
+        $this->connection->executeQuery('TRUNCATE TABLE user');
+        $this->connection->executeQuery('SET foreign_key_checks = 1');
+    }
+
     public function load(ObjectManager $manager): void
     {
+        // On TRUNCATE manuellement
+        $this->truncate();
+
         $faker = Factory::create('fr_FR');
-
-        $user = new User();
-        $user->setEmail('test@test.com');
-        $user->setAlias('test');
-        $user->setPassword('$2y$13$Tg1.AyawGux8ykl.DpBCluOasX7EWXRrwLPcsZg8CzI5w2rxBQ.Bm');
-        $user->setRoles(["ROLE_USER"]);
-        $manager->persist($user);
-
-        $user = new User();
-        $user->setEmail('user@user.com');
-        $user->setAlias('user');
-        $user->setPassword('$2y$13$NgnJKCnuzJ0UQwt1zkuTAOU8LlgHahmi6bEo/vWZF8jbVoUfxDIpC');
-        $user->setRoles(["ROLE_USER"]);
-        $manager->persist($user);
-
-        $user = new User();
-        $user->setEmail('admin@admin.com');
-        $user->setAlias('admin');
-        $user->setPassword('$2y$13$vMnkj4LRxWckp/O251JkBueRG8z6nPTwODUI5hT13Sd8TwUqRolbK');
-        $user->setRoles(["ROLE_ADMIN"]);
-        $manager->persist($user);
 
         $genresList = [];
         for ($g = 1; $g <= 10; $g++) {
@@ -57,6 +62,7 @@ class AppFixtures extends Fixture
             $manager->persist($author);
         }
 
+        $booksList = [];
         for ($b = 1; $b <= 10; $b++) {
             $book = new Book();
             $book->setTitle($faker->sentence());
@@ -79,15 +85,61 @@ class AppFixtures extends Fixture
                 $book->addGenre($randomGenre);
             }
 
-            $library = new Library();
-            $library->setUser($user);
-            $library->setBook($book);
-            $library->setFinished(true);
-            $manager->persist($library);
+            $booksList[] = $book;
 
             $manager->persist($book);
         }
 
+        $user = new User();
+        $user->setEmail('user@user.com');
+        $user->setAlias('user');
+        $user->setPassword('$2y$13$Tg1.AyawGux8ykl.DpBCluOasX7EWXRrwLPcsZg8CzI5w2rxBQ.Bm');
+        $user->setRoles(["ROLE_USER"]);
+        $manager->persist($user);
+
+        for ($i=0; $i <= mt_rand(1, 3); $i++) { 
+            $randomBook = $faker->unique()->randomElement($booksList);
+            $library = new Library();
+            $library->setUser($user);
+            $library->setBook($randomBook);
+            $library->setComment($faker->text());
+            $library->setQuote($faker->text());
+            $library->setRate(mt_rand(0,5));
+            $library->setFavorite($faker->boolean());
+            $library->setPurchased($faker->boolean());
+            $library->setWishlist($faker->boolean());
+            $library->setFinished($faker->boolean());
+            $manager->persist($library);
+        }
+
+        $test = new User();
+        $test->setEmail('test@test.com');
+        $test->setAlias('test');
+        $test->setPassword('$2y$13$NgnJKCnuzJ0UQwt1zkuTAOU8LlgHahmi6bEo/vWZF8jbVoUfxDIpC');
+        $test->setRoles(["ROLE_USER"]);
+        $manager->persist($test);
+
+        for ($i=0; $i <= mt_rand(1, 3); $i++) { 
+            $randomBook = $faker->unique()->randomElement($booksList);
+            $library = new Library();
+            $library->setUser($test);
+            $library->setBook($randomBook);
+            $library->setComment($faker->text());
+            $library->setQuote($faker->text());
+            $library->setRate(mt_rand(0,5));
+            $library->setFavorite($faker->boolean());
+            $library->setPurchased($faker->boolean());
+            $library->setWishlist($faker->boolean());
+            $library->setFinished($faker->boolean());
+            $manager->persist($library);
+        }
+
+        $admin = new User();
+        $admin->setEmail('admin@admin.com');
+        $admin->setAlias('admin');
+        $admin->setPassword('$2y$13$vMnkj4LRxWckp/O251JkBueRG8z6nPTwODUI5hT13Sd8TwUqRolbK');
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($admin);
 
 
         $manager->flush();
