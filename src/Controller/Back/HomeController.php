6d3366661e27efab41d5e1c4bd88f93ced4c96c6
Sandy-Bouzid <bouzid.sandy@gmail.com>
2023-08-11 08:38:37 +0200

    backoffice home & login redirection

--- src/Controller/Back/HomeController.php
+++ src/Controller/Back/HomeController.php
@@ -3,18 +3,23 @@
 namespace App\Controller\Back;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("back/", name="app_back_home")
+     * @Route("/", name="app_back_home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('back/home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        if($this->isGranted('IS_AUTHENTICATED_FULLY')){
+            return $this->render('back/home/index.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        };
+
+        return $this->redirectToRoute('app_login');
     }
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -49,7 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_back_book_index'));
+        return new RedirectResponse($this->urlGenerator->generate('app_back_home'));
     }
 
     protected function getLoginUrl(Request $request): string
