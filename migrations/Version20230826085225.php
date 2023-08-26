177aa82a9836aaa2754d96d1d1620065fb9b0a7a
Laurent-Finana <laurentfinanago@gmail.com>
2023-08-26 11:01:02 +0200

    install bundle and maker reset-password

--- migrations/Version20230826085225.php
+++ migrations/Version20230826085225.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230822064035 extends AbstractMigration
+final class Version20230826085225 extends AbstractMigration
 {
     public function getDescription(): string
     {
